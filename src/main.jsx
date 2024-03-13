c4b8d720577e376628e991f95a95d7d51b314334
OscarDelPrado19 <132949470+OscarDelPrado19@users.noreply.github.com>
2024-03-13 09:06:52 -0600

    si salo kri
    xd

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Navegador from './navegador.jsx'
 
-
+//Si jala kri
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <Navegador />
