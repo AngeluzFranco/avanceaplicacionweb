ee201f8c391a7eb3d541b5a39343925c4313bdde
AngeluzFranco <20223TN093@utez.edu.mx>
2024-03-26 19:09:37 -0600

    Update VistaMesas.jsx

--- src/PantallasAdmin/VistaMesas.jsx
+++ src/PantallasAdmin/VistaMesas.jsx
@@ -1,67 +1,74 @@
-import React, {useState} from 'react';
-import { Button, Navbar, Card, Modal, TextInput, Dropdown  } from 'flowbite-react';
-import fondo from '../assets/fondo.png';
+import React, { useState, useEffect } from 'react';
+import { Button, Card, Modal, TextInput, Dropdown } from 'flowbite-react';
 import imgMesa from '../assets/imgMesa.png';
 
 function VistaMesas() {
+  const [data, setData] = useState(null);
   const [crearOpen, setcrearOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://192.168.100.29:8080/api/gastromanager/mesas/');
+        if (!response.ok) {
+          throw new Error('Hubo un error en la petición');
+        }
+        const jsonData = await response.json();
+        setData(jsonData.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="h-screen ">
-<div className='flex justify-center mt-3 mb-5'>
-    <div className='w-9/12 m-2 flex justify-between'>
-        <Button onClick={() => setcrearOpen(true)} className='justify-start text-white bg-gradient-to-br from-red-500 to-orange-400 enabled:hover:bg-gradient-to-bl focus:ring-4 focus:ring-red-200 dark:focus:ring-red-800' size="xl" outline > Agregar  +  </Button>
-        <Dropdown label="TODAS">
+      <div className='flex justify-center mt-3 mb-5'>
+        <div className='w-9/12 m-2 flex justify-between'>
+          <Button onClick={() => setcrearOpen(true)} className='justify-start text-white bg-gradient-to-br from-red-500 to-orange-400 enabled:hover:bg-gradient-to-bl focus:ring-4 focus:ring-red-200 dark:focus:ring-red-800' size="xl" outline > Agregar  +  </Button>
+          <Dropdown label="TODAS">
             <Dropdown.Item>Ocupadas</Dropdown.Item>
             <Dropdown.Divider />
             <Dropdown.Item>Desocupadas</Dropdown.Item>
-        </Dropdown>
-    </div>
-</div>
+          </Dropdown>
+        </div>
+      </div>
 
       <div className="container-cards flex items-center justify-center flex-wrap">
-      <Card href="#" className="max-w-sm mx-5 my-5" style={{ background: '#ea4242' }}>
-          <div class="flex items-center gap-4">
-            <div className='mx-2'>
-              <div className='flex flex-row'>
-                <h5 class="text-xl font-bold tracking-tight text-white">
-                  Mesa:
-                </h5>
-                <h5 class="text-xl font-bold tracking-tight text-white">
-                  1
-                </h5>
+      {data && data.map((item, index) => (
+          <Card href="#" className="max-w-sm mx-5 my-5" style={{ background: '#fff', border: '1px solid orange' }} key={index}>
+            <div className="flex items-center gap-4">
+              <div className='mx-2'>
+                <div className='flex flex-row'>
+                  <h5 className="text-xl font-bold tracking-tight">Mesa:</h5>
+                  <h5 className="text-xl font-bold tracking-tight">{item.numeroMesa}</h5>
+                </div>
+                <div className='flex flex-col'>
+                  <p className="font-normal">Asientos:</p>
+                  <h5 className="text-xl font-bold tracking-tight">X {item.numeroSillas}</h5>
+                </div>
+                <div className='flex flex-row'>
+                  <h5 className="text-s font-bold tracking-tight">Estado:</h5>
+                  <h5 className="text-s font-bold tracking-tight">{item.estado}</h5>
+                </div>
               </div>
-              <div className='flex flex-col'>
-              <p class="font-normal text-white">
-                Asientos:
-              </p>
-              <h5 class="text-xl font-bold tracking-tight text-white">
-                X 5
-              </h5>
-              </div>
-              <div className='flex flex-row'>
-              <h5 class="text-s font-bold tracking-tight text-white">
-                Estado:
-              </h5>
-              <h5 class="text-s font-bold tracking-tight text-white">
-                Desocupada
-              </h5>
+              <div className="flex flex-col items-center">
+                <img src={imgMesa} className="mb-2 h-20 md:h-20 sm:h-20" alt="imagen mesa" />
+                <Button className="mt-4 bg-gradient-to-br from-red-500 to-orange-400 enabled:hover:bg-gradient-to-bl focus:ring-4 focus:ring-red-200 dark:focus:ring-red-800" style={{ width: 50 }} onClick={() => handleAbrirModal(item)}>estado</Button>
               </div>
             </div>
-            <div class="flex flex-col items-center">
-              <img src={imgMesa} class="mb-2 h-20 md:h-20 sm:h-20" alt="imagen mesa" />
-              <Button className=" mt-4  text-white bg-gradient-to-br from-red-500 to-orange-400 enabled:hover:bg-gradient-to-bl focus:ring-4 focus:ring-red-200 dark:focus:ring-red-800" style={{ width: 50 }}>estado</Button>
-            </div>
-          </div>
-        </Card>
+          </Card>
+        ))}
       </div>
 
 
       <Modal show={crearOpen} onClose={() => setcrearOpen(false)} size="md" position="center">
-<Modal.Header style={{ padding: 0 }}>
-  <div className="flex items-center justify-center w-full">
-    <h5 className="text-xl font-medium text-gray-900 dark:text-white text-center">Crea tu mesa</h5>
-  </div>
-</Modal.Header>
+        <Modal.Header style={{ padding: 0 }}>
+          <div className="flex items-center justify-center w-full">
+            <h5 className="text-xl font-medium text-gray-900 dark:text-white text-center">Crea tu mesa</h5>
+          </div>
+        </Modal.Header>
         <Modal.Body>
           <div className="text-center">
             <form>
