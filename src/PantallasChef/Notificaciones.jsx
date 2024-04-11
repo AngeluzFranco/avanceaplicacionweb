7eb5c6bda9bafa0013642a0ce8e368e70dbde841
AngeluzFranco <20223TN093@utez.edu.mx>
2024-04-10 18:42:33 -0600

    gsgs

--- src/PantallasChef/Notificaciones.jsx
+++ src/PantallasChef/Notificaciones.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Card } from 'flowbite-react';
+import { Card } from 'flowbite-react'; // Supongo que "Button" no se usa en este componente
 import { API_BASE_URL } from '../backend.js';
 import Swal from 'sweetalert2';
 
@@ -18,13 +18,19 @@ function Notificaciones() {
         setData(jsonData.data);
       } catch (error) {
         console.error(error);
+        // Aquí podrías mostrar una alerta usando Swal
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Hubo un error en la petición',
+        });
       }
     };
     fetchData();
   }, []);
 
   return (
-    <div className="h-screen ">
+    <div className="h-screen">
       <div className="container-cards flex items-start" style={{ height: 'auto', marginTop: '2%' }}>
         <div className="flex flex-col items-center" style={{ width: '100%' }}>
           {data && data.map((notificacion, index) => (
@@ -33,20 +39,20 @@ function Notificaciones() {
               className="max-w-screen-xl w-full mb-4"
             >
               <h5 className="text-md font-bold tracking-tight text-gray-900 dark:text-white">
-                {notificacion.fecha}
+                Fecha: {notificacion.fecha}
               </h5>
               <p className="font-normal text-gray-700 dark:text-gray-400">
-                {notificacion.descripcion}
+                Descripción: {notificacion.descripcion}
               </p>
               <p className="font-normal text-gray-700 dark:text-gray-400">
-                {notificacion.tipo}
+                Tipo: {notificacion.tipo}
               </p>
             </Card>
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Notificaciones
+export default Notificaciones;
--- src/PantallasChef/VistaPedidos.jsx
+++ src/PantallasChef/VistaPedidos.jsx
@@ -11,11 +11,11 @@ function RecVisualizarP() {
       try {
         const url = `${API_BASE_URL}/detallepedido/`;
         const token = localStorage.getItem('token');
-        const response = await fetch(url,{
+        const response = await fetch(url, {
           headers: {
-            'Authorization' : `Bearer ${token}`
+            'Authorization': `Bearer ${token}`
           }
-      });
+        });
         if (!response.ok) {
           throw new Error('Hubo un error en la petición');
         }
@@ -28,7 +28,7 @@ function RecVisualizarP() {
     fetchData();
   }, []);
 
-  const handleTerminarPedido = () => {
+  const handleTerminarPedido = async (pedidoId) => {
     Swal.fire({
       title: '¿Estás seguro?',
       text: 'Una vez Terminado el pedido, no se puede deshacer.',
@@ -37,54 +37,96 @@ function RecVisualizarP() {
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Sí, terminar pedido'
-    }).then((result) => {
+    }).then(async (result) => {
       if (result.isConfirmed) {
-        // la lógica para cambio de estado
-        Swal.fire(
-          '¡Pedido terminado!',
-          'La confirmacion del pedido se ha realizado correctamente.',
-          'success'
-        );
-        closeModal();
-      }
-      else {
-        closeModal();
+        try {
+          const url = `${API_BASE_URL}/pedidos/${pedidoId}`; // Ajusta la URL según la estructura de tu API
+          const token = localStorage.getItem('token');
+          const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+              estado: 'Terminado'
+            })
+          });
+  
+          if (!response.ok) {
+            throw new Error('Hubo un error al intentar cambiar el estado del pedido');
+          }
+  
+          // Actualizar los datos manteniendo los detalles del pedido
+          const updatedPedido = await response.json();
+          setData(data.map(pedido => pedido.idPedido === pedidoId ? updatedPedido : pedido));
+  
+          Swal.fire(
+            '¡Pedido terminado!',
+            'La confirmación del pedido se ha realizado correctamente.',
+            'success'
+          );
+        } catch (error) {
+          console.error(error);
+          Swal.fire(
+            'Error',
+            'Hubo un error al intentar cambiar el estado del pedido.',
+            'error'
+          );
+        }
       }
     });
+  };
+  
+
+  // Función para agrupar los detalles del pedido por ID de pedido
+  const groupByPedidoId = () => {
+    if (!data) return [];
+
+    const groupedData = {};
+    data.forEach(detallePedido => {
+      const pedidoId = detallePedido.pedido.idPedido;
+      if (!groupedData[pedidoId]) {
+        groupedData[pedidoId] = {
+          platillos: [],
+          estado: detallePedido.pedido.estado
+        };
+      }
+      groupedData[pedidoId].platillos.push(detallePedido.platillo.nombre);
+    });
+    return groupedData;
   };
 
   return (
     <div className="h-screen">
       <div className="container-cards flex items-start" style={{ height: 'auto', marginTop: '2%' }}>
-        <div className="flex flex-col items-center" style={{ width: '100%' }} >
-          {data && data.map((pedido, index) => (
-            <Card
-              key={index}
-              className="max-w-screen-xl w-full mb-4"
-            >
-              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <div style={{ flexDirection: 'colum' }}>
-                  <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Pedido: {pedido.idPedido}
-                  </h5>
-                  <h4 className="text-xl font-bold tracking-tight text-gray-800 dark:text-white">
-                    Para la mesa: {pedido.mesa.numeroMesa}
-                  </h4>
-                </div>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  Platillos: {pedido.detallesPedidoBean.map(detalle => detalle.platillo.nombre).join(', ')}
-                </p>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  Estado: {pedido.estado}
-                </p>
-                <Button onClick={handleTerminarPedido}>Terminado</Button>
-              </div>
-            </Card>
-          ))}
+        <div className="flex flex-col items-center" style={{ width: '100%' }}>
+        {Object.keys(groupByPedidoId())
+  .filter(pedidoId => groupByPedidoId()[pedidoId].estado === 'En proceso')
+  .map(pedidoId => (
+    <Card key={pedidoId} className="max-w-screen-xl w-full mb-4">
+      <div className="flex justify-between">
+        <div>
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Pedido: {pedidoId}
+          </h5>
+        </div>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Platillos: {groupByPedidoId()[pedidoId].platillos.join(', ')}
+        </p>
+        <div><p className="font-normal text-gray-700 dark:text-gray-400">
+          Estado: {groupByPedidoId()[pedidoId].estado}
+        </p></div>
+        <div>
+          <Button onClick={() => handleTerminarPedido(pedidoId)}>Terminado</Button>
+        </div>
+      </div>
+    </Card>
+))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default RecVisualizarP;
